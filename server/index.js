6935b870f2cdb1292879bbe8e4f9812ca0960649
Agash <agash@mail.com>
2024-03-26 22:47:33 +0530

    1st graphql

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const colors = require("colors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const db = require("./config/db");
+
+const port = process.env.PORT | 5000;
+const app = express();
+db();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "D",
+  })
+);
+
+app.listen(port, console.log(`Server runs on ${port}`));
